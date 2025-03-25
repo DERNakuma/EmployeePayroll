c78b5653442c713fe7c1a5ada01ac5b2ccb9301e
DERNakuma <krishnasinghrajput7878@gmail.com>
2025-03-25 23:51:12 +0530

    CRUD Operation with RestApi

---
+++ src/main/java/com/example/EmployeePayrollApp/model/Employee.java
@@ -0,0 +1,23 @@
+package com.example.EmployeePayrollApp.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Employees")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String department;
+    private double salary;
+}
